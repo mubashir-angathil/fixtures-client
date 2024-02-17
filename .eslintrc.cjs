e12b61f5d2a92c78a55905f392956b2f792e273c
Mubashir Angathil <mubashirangathil5142@gmail.com>
2024-02-17 16:35:15 +0530

    Project initialization

---
+++ .eslintrc.cjs
@@ -0,0 +1,34 @@
+module.exports = {
+  root: true,
+  env: { browser: true, es2020: true },
+  extends: [
+    'eslint:recommended',
+    'plugin:@typescript-eslint/recommended',
+    'plugin:react-hooks/recommended',
+  ],
+  ignorePatterns: ['dist', '.eslintrc.cjs'],
+  parser: '@typescript-eslint/parser',
+  plugins: ['react-refresh'],
+  rules: {
+    "react-refresh/only-export-components": [
+      "warn",
+      { allowConstantExport: true },
+    ],
+    // General ESLint Rules
+    "no-console": ["warn", { allow: ["error", "info"] }],
+    "no-unused-vars": "warn",
+
+    // TypeScript Rules
+    "@typescript-eslint/no-unused-vars": "warn",
+    "@typescript-eslint/explicit-module-boundary-types": "off",
+    "@typescript-eslint/no-explicit-any": "off",
+
+    // React Rules
+    "react/prop-types": "off", // We are using TypeScript for prop type checking
+    "react/display-name": "off",
+
+    // React Hooks Rules
+    "react-hooks/rules-of-hooks": "error",
+    "react-hooks/exhaustive-deps": "warn",
+  },
+}
